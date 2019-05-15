8a1fa61e897fd66a2645d2fd8b9949976c73b2ba
ReD$erpenT <R$@ciot135.local.net>
2019-05-15 11:21:15 +0300

    add script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mysql_backup
+
+***
+
+_make app passwords_
+[Googgle](https://myaccount.google.com/apppasswords)
+
+_add msmtprc to /etc_
+
---
+++ mysql_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+USER=user
+PASS=pass
+DB=`mysql -u $USER -p$PASS -e 'show databases;' | grep bitrix`
+OPT="--quick --insert-ignore --skip-lock-tables --single-transaction=TRUE"
+DEST=/backup
+DATE=`date +%Y-%m-%d-%H-%M`
+
+for i in $DB;
+do
+    mysqldump -u $USER -p$PASS $OPT -B $DB | /bin/gzip -c > $DEST/$DATE-$DB.sql.gz;
+done
+
+if [ -e $DEST/$DATE-$DB.sql.gz ]; then
+    echo "Backup $DEST/$DATE-$DB complete" >> /tmp/mail
+    cat /tmp/mail | msmtp -a default user@mail.ru
+fi
+
+find /backup -maxdepth 1 -name "*sql.gz" -mtime +5 -exec rm -f {} \;
