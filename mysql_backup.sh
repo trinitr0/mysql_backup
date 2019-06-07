020f74696c59a4cd2979779956705dae4c04f5b7
ReD$erpenT <R$@ciot135.local.net>
2019-06-07 13:18:41 +0300

    add fail

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -15,6 +15,10 @@ done
 if [ -e $DEST/$DATE-$DB.sql.gz ]; then
     echo "Backup $DEST/$DATE-$DB complete" >> /tmp/mail
     cat /tmp/mail | msmtp -a default user@mail.ru
+else
+    echo "Backup $DEST/$DATE-$DB FAIL!" >> /tmp/mail
+    cat /tmp/mail | msmtp -a default user@mail.ru
 fi
+
 
 find /backup -maxdepth 1 -name "*sql.gz" -mtime +5 -exec rm -f {} \;
