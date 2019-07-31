3b7ebb3430c2d6bc901db706337a6d36e013aec7
ReD$erpenT <R$@ciot135.local.net>
2019-07-31 09:43:57 +0300

    add new variable

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -4,6 +4,7 @@ USER=user
 PASS=pass
 DB=`mysql -u $USER -p$PASS -e 'show databases;' | grep bitrix`
 OPT="--quick --insert-ignore --skip-lock-tables --single-transaction=TRUE"
+BMAIL=/tmp/mail
 DEST=/backup
 DATE=`date +%F`
 
@@ -13,16 +14,16 @@ do
 done
 
 if [ -e $DEST/$DATE-$DB.sql.gz ]; then
-    echo "Backup $DEST/$DATE-$DB complete" >> /tmp/mail
-    cat /tmp/mail | msmtp -a default user@mail.ru
+    echo "Backup $DEST/$DATE-$DB complete" >> $BMAIL
+    cat $BMAIL | msmtp -a default user@mail.ru
 else
-    echo "Backup $DEST/$DATE-$DB FAIL!" >> /tmp/mail
-    cat /tmp/mail | msmtp -a default user@mail.ru
+    echo "Backup $DEST/$DATE-$DB FAIL!" >> $BMAIL
+    cat $BMAIL | msmtp -a default user@mail.ru
 fi
 
 
 find /backup -maxdepth 1 -name "*sql.gz" -mtime +5 -exec rm -f {} \;
 
-if [ $(cat /tmp/mail | wc -l) > 9 ]; then
- sed -i '5d' /tmp/mail
+if [ $(cat $BMAIL | wc -l) > 9 ]; then
+ sed -i '5d' $BMAIL
 fi
